4cde82f0e7e66c90674098c4ad6a7925465f7942
nicole_s <15011784+nicolelaine@users.noreply.github.com>
2023-07-25 19:18:47 +0200

    Added if statement and innerText

--- js/script.js
+++ js/script.js
@@ -1,5 +1,13 @@
 var button = document.querySelector(".show-cat");
 var cat = document.querySelector(".cat");
 button.addEventListener("click", function () {
-cat.classList.add("show");
+if (cat.classList.contains("show")) {
+    cat.classList.remove("show");
+    button.innerText = "Wait, come back!!"
+    button.classList.remove("disappear")
+} else {
+    cat.classList.add("show");
+    button.innerText = "Shoo, cat!!"
+    button.classList.add("disappear")
+}
 });
